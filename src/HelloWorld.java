bf9959524b2fe63bf0466f9dd5ec2533b7d662f6
César MILESI <cesar.milesi@epita.fr>
2021-05-17 15:44:52 +0200

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HelloWorld
+{
+    public static void main(String[]args)
+    {
+        //Scanner scanner = new Scanner(System.in);
+        //scanner.
+        System.out.println("Hello World");
+    }
+}
+
